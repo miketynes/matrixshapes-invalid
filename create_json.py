b55ba2525a156cebbc680d13fa6cf9a747021b5f
miketynes <mtynes@fordham.edu>
2022-10-25 05:47:54 -0500

    default to 100 examples

--- create_json.py
+++ create_json.py
@@ -9,7 +9,7 @@ def parse_args():
     # Parse command line arguments
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--num", type=int, default=1000, help="Task examples to generate.")
+    parser.add_argument("--num", type=int, default=100, help="Task examples to generate.")
     parser.add_argument("--cont", type=float, default=0.5, help="Percentage of how many examples are allowed to contain the label.")
     parser.add_argument("--frac_invalid", type=float, default=0.5)
     parser.add_argument("--num_ops", type=int, default=5)
